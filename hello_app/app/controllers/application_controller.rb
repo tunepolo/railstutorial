48d7444207240e827ea0e608983a853f11f84d2b
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-07 21:45:53 +0900

    演習2を実施
    「¡Hola, mundo!」を使った非ASCII文字の表示確認

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hola, mundo!"
+    render html: "¡Hola, mundo!"
   end
 end
