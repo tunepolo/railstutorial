8c81adaec3a14c74658c49ce6e431e0611fb3a9f
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-08 20:37:31 +0900

    rootアクセス時にユーザ一覧が表示されるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'application#hello'
+  root 'users#index'
 end
