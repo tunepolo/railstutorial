0fc1f53e8eb3b13d92644f5480b827275f50b78f
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-12 15:12:39 +0900

    ユーザ登録成功時にユーザページへリダイレクトする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存に成功した時
+      redirect_to @user
     else
       render 'new'
     end
