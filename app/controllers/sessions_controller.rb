7ce6ec201ea47b2e6d2fb386fca832fab61355f6
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-12 20:28:59 +0900

    ログイン認証処理を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザログイン成功。ユーザ情報のページへリダイレクト
+    else
+      # ユーザログイン失敗。エラーメッセージを作成する。
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
