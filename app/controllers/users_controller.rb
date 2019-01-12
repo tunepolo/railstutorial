b741c27c0bf645f82cc02cd7463f42464f4a774a
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-12 14:44:34 +0900

    ユーザフォームを動くようにし、エラー画面も対応。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存に成功した時
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
