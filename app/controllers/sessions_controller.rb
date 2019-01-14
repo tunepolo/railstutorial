d64231d486577f3e93e7e30a9f3a421fb056ba7c
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-14 18:45:05 +0900

    remember_tokenの内容を確認できるようにテストを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       # ユーザログイン成功。ユーザ情報のページへリダイレクト
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # ユーザログイン失敗。エラーメッセージを作成する。
       flash.now[:danger] = 'Invalid email/password combination'
