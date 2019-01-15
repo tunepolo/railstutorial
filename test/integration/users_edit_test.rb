c07b4751f0eaa2c27ca78aecbd7b56253e9dccef
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-15 20:48:47 +0900

    編集失敗時のエラーメッセージのチェックを追加

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -14,6 +14,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                             password: "foo",
                             password_confirmation: "bar" } }
     assert_template 'users/edit'
+    assert_select "div.alert-danger", "The form contains 4 errors"
   end
 
 end
