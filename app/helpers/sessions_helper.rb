6dd128b228db07596a7ad542177bf054c9f470f2
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-15 21:15:46 +0900

    ログインユーザと編集対象ユーザが違う場合にroot_urlへリダイレクトするチェックを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # 渡されたユーザがログイン済みユーザであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
   # 現在ログイン中のユーザを返す
   def current_user
     if (user_id = session["user_id"])
