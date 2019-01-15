fa02b14bbfc160e44a8f5d247db7a81a697f8d0a
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-15 21:06:37 +0900

    編集・更新がログインユーザのみ動くようにbefore_actionを設定

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
@@ -18,6 +19,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Foo Bar"
@@ -31,6 +33,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                            email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
 end
