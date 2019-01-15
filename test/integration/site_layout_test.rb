fdc10784c06b3140be66830f3b1c5a620dd3a051
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-15 21:39:56 +0900

    レイアウトにあるすべてのリンクに対して統合テストにチェックを追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -13,6 +17,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+
+  test "layout links when logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 
 end
