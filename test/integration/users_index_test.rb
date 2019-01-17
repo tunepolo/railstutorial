7118fae9a28bf25b5954db7a91287e5b5fa13b1b
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-17 21:16:07 +0900

    できていなかった最後の演習を対応

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
+    @non_activated = users(:non_activated)
   end
 
   test "index as admin including pagination and delete links" do
@@ -28,6 +29,15 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "non activated user not listed in index" do
+    log_in_as(@non_activated)
+    assert_not @non_activated.activated?
+    get users_path
+    assert_redirected_to login_url
+    get user_path(@non_activated)
+    assert_redirected_to root_url
   end
 
 end
