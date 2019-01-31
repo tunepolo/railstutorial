1bdef96c591dc21c2187ebe36d4f79494fc75e69
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-31 21:41:15 +0900

    ブロックの前後に空行を挟むように修正 (refs #2)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20190120062913) do
-
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -35,5 +34,4 @@ ActiveRecord::Schema.define(version: 20190120062913) do
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
