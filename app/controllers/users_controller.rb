1730d82d4912eba1c278204dbfaaa2e440bbd07a
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-02 17:29:50 +0900

    Micropostコントローラにcreateとdestroyの雛形を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,15 +56,6 @@ class UsersController < ApplicationController
 
     # beforeアクション
 
-    # ログイン済みユーザかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     # 正しいユーザか確認
     def correct_user
       @user = User.find(params[:id])
