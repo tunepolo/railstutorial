e478955b66b4dcfda5d8c0c1a5dc9d2de327027c
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-03 10:31:40 +0900

    rubocopで指摘されたインデントの狂いを修正 (refs #2)

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -21,14 +21,12 @@ class MicropostsController < ApplicationController
 
   private
 
-    def micropost_params
-      params.require(:micropost).permit(:content)
-    end
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
 
-    def correct_user
-      @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @micropost.nil?
-
-    end
-
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
 end
