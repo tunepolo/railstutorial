308ff440cdefc22e97e4a2f4ebbfa1eae28f40c9
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-03 10:49:20 +0900

    Micropost投稿に関する統合テストを追加し、見つかったバグを合わせて修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-      @feed_item = []
+      @feed_items = []
       render 'static_pages/home'
     end
   end
