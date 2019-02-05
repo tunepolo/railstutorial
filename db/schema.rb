09a3c27511ae747f335da410240f7702c7a59d21
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-06 07:38:26 +0900

    rubocopの警告対応

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20190205223028) do
-
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -27,7 +26,8 @@ ActiveRecord::Schema.define(version: 20190205223028) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index %w[follower_id followed_id], name: "index_relationships_on_follower_id_and_followed_id",
+                                         unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
@@ -46,5 +46,4 @@ ActiveRecord::Schema.define(version: 20190205223028) do
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
