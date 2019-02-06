71c02f84c680ee8b2bf42cf66534ea8293c7bbb8
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-06 11:27:04 +0900

    rubocopの指摘事項を修正

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class RelationshipTest < ActiveSupport::TestCase
   def setup
     @relationship = Relationship.new(follower_id: users(:michael).id,
-                      followed_id: users(:archer).id)
+                                     followed_id: users(:archer).id)
   end
 
   test "should be valid" do
