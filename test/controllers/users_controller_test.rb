63eb32325aa5d5652f285fff842b8c26fac29416
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-09 19:56:54 +0900

    ハッシュのインデントを修正 (refs #2)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -42,9 +42,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
-                                    user: { password: @other_user.password,
-                                            password_confirmation: @other_user.password_confirmation,
-                                            admin: true } }
+      user: { password: @other_user.password,
+              password_confirmation: @other_user.password_confirmation,
+              admin: true } }
     assert_not @other_user.reload.admin?
   end
 
