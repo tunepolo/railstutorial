4989b3f40c41854082ecc28f98592a67b5234168
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-09 20:11:11 +0900

    get_から始まるprefixを削除 (refs #2)

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,5 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user, only: %i[edit update]
+  before_action :load_user, only: %i[edit update]
   before_action :valid_user, only: %i[edit update]
   before_action :check_expiration, only: %i[edit update]
 
@@ -40,7 +40,7 @@ class PasswordResetsController < ApplicationController
     params.require(:user).permit(:password, :password_confirmation)
   end
 
-  def get_user
+  def load_user
     @user = User.find_by(email: params[:email])
   end
 
