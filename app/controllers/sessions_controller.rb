84789f1507d96440effdb8bb8d68a627473f6327
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-16 08:27:18 +0900

    不要なreturnを削除

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,12 +23,12 @@ class SessionsController < ApplicationController
     if user.activated?
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      return redirect_back_or user
+      redirect_back_or user
     else
       message = 'Account not activated.'
       message += 'Check your email for the activation link.'
       flash[:warning] = message
-      return redirect_to root_url
+      redirect_to root_url
     end
   end
 end
