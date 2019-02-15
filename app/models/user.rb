8f8ae2f8b8701750d67d54eb51fc4ae7ae85d54f
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-02-16 08:11:09 +0900

    複数行にわたる3項演算子の使用をやめた (refs #2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,11 @@ class User < ApplicationRecord
 
   # 渡された文字列のハッシュ値を返す
   def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                              BCrypt::Engine.cost
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
     BCrypt::Password.create(string, cost: cost)
   end
 
